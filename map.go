aee9cd5310135df9b0121d2a0a9509c5a7051d11
kiwi <qiweizhang8523@gmail.com>
2018-01-30 23:15:52 +0800

    finsh map

--- map.go
+++ map.go
@@ -12,6 +12,16 @@ func main() {
 	v1:=m["k1"]
 	fmt.Println("v1:",v1)
 
-	fmt 
+	fmt.Println("len:",len(m))
+
+	delete(m,"k2")
+	fmt.Println("map",m)
+	
+	_,prs:=m["k2"]
+	fmt.Println("prs:",prs)
+
+	n:=map[string]int{"foo":1,"bar":2}
+	fmt.Println("map",n)
+
 
 }
